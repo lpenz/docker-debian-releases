fa3945df197d12eea6f4a3439101d291de7788c1
Leandro Lisboa Penz <lpenz@lpenz.org>
2019-07-13 21:42:59 +0100

    Get args for travis-deploy.sh from environment
    That's what the travis-* script do.

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-IMAGE="$1"
-DESCRIPTION="$2"
+if [ -z "$IMAGE" ]; then
+    echo Unspecified IMAGE >&2
+    exit 1
+fi
+
+if [ -z "$DESCRIPTION" ]; then
+    echo Unspecified DESCRIPTION >&2
+    exit 1
+fi
 
 set -e -x
 
 docker push "$IMAGE"
 ./dockerhub-set-descriptions "$IMAGE" "$DESCRIPTION" "Automatically updated via https://github.com/lpenz/docker-debian-releases/"
+
