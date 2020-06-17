f9860844311117631e8d4f9dac28a586d3ee31df
Leandro Lisboa Penz <lpenz@lpenz.org>
2020-06-17 20:21:44 +0100

    Refactor bring-random-branch
    - Process all release infos into newBranches and movBranches
    - If newBranches, randomly pick one codename
    - Otherwise, do the same on movBranches

--- go/cmd/bring-random-branch/main.go
+++ go/cmd/bring-random-branch/main.go
@@ -30,49 +30,17 @@ func cmdLineParse() (string, error) {
 	return os.Args[1], nil
 }
 
-func main() {
-	srcfilename, err := cmdLineParse()
-	common.PanicIf(err)
-	head := gitRevParse("HEAD")
-	if head == "" {
-		panic("invalid HEAD")
-	}
-	releaseInfos := common.ReadReleaseInfo(srcfilename)
-	options := []common.ReleaseInfo{}
-	foundNew := false
+func bringBranchesCodename(releaseInfos *[]common.ReleaseInfo, codename *string) {
+	done := map[string]bool{}
 	for _, ri := range *releaseInfos {
-		for _, branchname := range ri.BranchNames() {
-			if branchname == "" {
-				continue
-			}
-			rev := gitRevParse(fmt.Sprintf("origin/%s", branchname))
-			if rev == "" {
-				// Found brand new release, process it only
-				fmt.Printf("New release found! %s\n", branchname)
-				foundNew = true
-				options = []common.ReleaseInfo{ri}
-				break
-			}
-			if rev != head {
-				options = append(options, ri)
-				break
-			}
-		}
-		if foundNew {
-			break
-		}
-	}
-	if len(options) == 0 {
-		return
-	}
-	rand.Seed(time.Now().Unix())
-	chosen := options[rand.Intn(len(options))]
-	for _, ri := range *releaseInfos {
-		if ri.Origin != ri.Origin || ri.Codename != chosen.Codename {
+		if ri.Codename != *codename {
 			continue
 		}
 		for _, branchname := range ri.BranchNames() {
 			if branchname == "" {
+				continue
+			}
+			if _, ok := done[branchname]; ok {
 				continue
 			}
 			cmdstr := []string{"git", "push", "-f", "origin", fmt.Sprintf("HEAD:%s", branchname)}
@@ -82,6 +50,51 @@ func main() {
 			cmd.Stderr = os.Stderr
 			err := cmd.Run()
 			common.PanicIf(err)
+			done[branchname] = true
 		}
 	}
+}
+
+func main() {
+	srcfilename, err := cmdLineParse()
+	common.PanicIf(err)
+	head := gitRevParse("HEAD")
+	if head == "" {
+		panic("invalid HEAD")
+	}
+	releaseInfos := common.ReadReleaseInfo(srcfilename)
+	movBranches := []common.ReleaseInfo{}
+	newBranches := []common.ReleaseInfo{}
+	for _, ri := range *releaseInfos {
+		for _, branchname := range ri.BranchNames() {
+			if branchname == "" {
+				continue
+			}
+			rev := gitRevParse(fmt.Sprintf("origin/%s", branchname))
+			if rev == "" {
+				// Found brand new release, process it only
+				fmt.Printf("New release found! %s\n", branchname)
+				newBranches = append(newBranches, ri)
+				break
+			}
+			if rev != head {
+				movBranches = append(movBranches, ri)
+				break
+			}
+		}
+	}
+	rand.Seed(time.Now().Unix())
+	var label string = ""
+	var set *[]common.ReleaseInfo = nil
+	if len(newBranches) > 0 {
+		label = "NEW"
+		set = &newBranches
+	} else if len(movBranches) > 0 {
+		label = "OLD"
+		set = &movBranches
+	}
+	i := rand.Intn(len(*set))
+	chosen := (*set)[i].Codename
+	fmt.Printf("Bringing %s codename %s at index %d\n", label, chosen, i)
+	bringBranchesCodename(releaseInfos, &chosen)
 }
